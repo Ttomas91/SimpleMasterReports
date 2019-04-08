2896e5f6e84d33c7adae2d37c6d3fad6ed0f6285
Ttomas91 <Ttom12@mail.ru>
2019-04-08 14:47:33 +0300

    fix tmp bug

--- lib/tools_menu.py
+++ lib/tools_menu.py
@@ -10,16 +10,48 @@ class tools_bar():
         self.icon_patch=main_path+'/icons/'
         mask_to=[[0 for y in range(11)]for x in range(2)]
 
-        mask_to[0][1]=[ImageTk.PhotoImage(file=self.icon_patch+'up_border_solid'+'.png'),ImageTk.PhotoImage(file=self.icon_patch+'up_border_null'+'.png'),self.f_bord_up]
-        mask_to[0][2]=[ImageTk.PhotoImage(file=self.icon_patch+'all_border_solid'+'.png'),ImageTk.PhotoImage(file=self.icon_patch+'all_border_null'+'.png'),self.f_bord_all]
+        self.but_border_up=self.factory_objects(
+            0,1,1,1,
+            ImageTk.PhotoImage(file=self.icon_patch+'up_border_null'+'.png'),
+            ImageTk.PhotoImage(file=self.icon_patch+'up_border_solid'+'.png'),
+            ImageTk.PhotoImage(file=self.icon_patch+'up_border_solid'+'.png'),
+            self.f_bord_up,)
+        
+        self.but_border_all=self.factory_objects(
+            0,2,1,1,
+            ImageTk.PhotoImage(file=self.icon_patch+'all_border_null'+'.png'),
+            ImageTk.PhotoImage(file=self.icon_patch+'all_border_solid'+'.png'),
+            ImageTk.PhotoImage(file=self.icon_patch+'all_border_solid'+'.png'),
+            self.f_bord_all,)
+
+        self.but_border_left=self.factory_objects(
+            1,0,1,1,
+            ImageTk.PhotoImage(file=self.icon_patch+'left_border_null'+'.png'),
+            ImageTk.PhotoImage(file=self.icon_patch+'left_border_solid'+'.png'),
+            ImageTk.PhotoImage(file=self.icon_patch+'left_border_solid'+'.png'),
+            self.f_bord_left,)
+
+        self.but_border_down=self.factory_objects(
+            1,1,1,1,
+            ImageTk.PhotoImage(file=self.icon_patch+'down_border_null'+'.png'),
+            ImageTk.PhotoImage(file=self.icon_patch+'down_border_solid'+'.png'),
+            ImageTk.PhotoImage(file=self.icon_patch+'down_border_solid'+'.png'),
+            self.f_bord_down,)
+
+        self.but_border_right=self.factory_objects(
+            1,2,1,1,
+            ImageTk.PhotoImage(file=self.icon_patch+'right_border_null'+'.png'),
+            ImageTk.PhotoImage(file=self.icon_patch+'right_border_solid'+'.png'),
+            ImageTk.PhotoImage(file=self.icon_patch+'right_border_solid'+'.png'),
+            self.f_bord_right,)
+
+        
+        
         mask_to[0][3]=[ImageTk.PhotoImage(file=self.icon_patch+'top_text_ax1_select'+'.png'),ImageTk.PhotoImage(file=self.icon_patch+'top_text_ax1'+'.png'),self.f_text_v_up]
         mask_to[0][4]=[ImageTk.PhotoImage(file=self.icon_patch+'centre_text_ax1_select'+'.png'),ImageTk.PhotoImage(file=self.icon_patch+'centre_text_ax1'+'.png'),self.f_text_v_centr]
         mask_to[0][5]=[ImageTk.PhotoImage(file=self.icon_patch+'bottom_text_ax1_select'+'.png'),ImageTk.PhotoImage(file=self.icon_patch+'bottom_text_ax1'+'.png'),self.f_text_v_down]
         mask_to[0][7]=[1,ImageTk.PhotoImage(file=self.icon_patch+'up'+'.png'),self.f_join_up]
         
-        mask_to[1][0]=[ImageTk.PhotoImage(file=self.icon_patch+'left_border_solid'+'.png'),ImageTk.PhotoImage(file=self.icon_patch+'left_border_null'+'.png'),self.f_bord_left]
-        mask_to[1][1]=[ImageTk.PhotoImage(file=self.icon_patch+'down_border_solid'+'.png'),ImageTk.PhotoImage(file=self.icon_patch+'down_border_null'+'.png'),self.f_bord_down]
-        mask_to[1][2]=[ImageTk.PhotoImage(file=self.icon_patch+'right_border_solid'+'.png'),ImageTk.PhotoImage(file=self.icon_patch+'right_border_null'+'.png'),self.f_bord_right]
         mask_to[1][3]=[ImageTk.PhotoImage(file=self.icon_patch+'left_text_ax0_select'+'.png'),ImageTk.PhotoImage(file=self.icon_patch+'left_text_ax0'+'.png'),self.f_text_h_left]
         mask_to[1][4]=[ImageTk.PhotoImage(file=self.icon_patch+'centre_text_ax0_select'+'.png'),ImageTk.PhotoImage(file=self.icon_patch+'centre_text_ax0'+'.png'),self.f_text_h_centr]
         mask_to[1][5]=[ImageTk.PhotoImage(file=self.icon_patch+'right_text_ax0_select'+'.png'),ImageTk.PhotoImage(file=self.icon_patch+'right_text_ax0'+'.png'),self.f_text_h_right]
@@ -30,7 +62,20 @@ class tools_bar():
         
         self.mask_tool=mask_to
 
-    def factory_objects
+    def factory_objects(self,ax,ay,len_ax,len_ay,i_0,i_1,i_2,func,*a_key):
+        ob={}
+        ob['ax']=ax
+        ob['ay']=ay
+        ob['len_ax']=len_ax
+        ob['len_ay']=len_ay
+        ob['icon_0']=i_0
+        ob['icon_1']=i_1
+        ob['icon_2']=i_2
+        ob['func']=func
+        for x in range(len(a_key)):
+            ob['a_key_'+str(x)]=a_key[x]
+        
+        return ob
 
     def f_bord_up(self,*key):
         if self.mask_tool[0][1][-2]==0:
